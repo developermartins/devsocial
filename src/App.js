13925b454b5460aade496048cc7e2e1e74089694
marts <martssilvapro@gmail.com>
2022-11-22 13:56:40 -0300

    implements register page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Register />
     </div>
   );
 }
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import './Register.scss';
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <section className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Voluptatem nam eius ut dicta aut pariatur voluptas reprehenderit 
+            repellat perspiciatis odit?
+          </p>
+          <span>Do you have an account?</span>
+          <button>Login</button>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder='Username' />
+            <input type="email" placeholder='Email' />
+            <input type="password" placeholder='Password' />
+            <input type="text" placeholder='Name' />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </section>
   );
 };
 
