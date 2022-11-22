f6f92892f6ae652a47cb94603c851c459234acd1
marts <martssilvapro@gmail.com>
2022-11-22 14:20:23 -0300

    import styles

--- src/components/LeftBar/LeftBar.jsx
+++ src/components/LeftBar/LeftBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './LeftBar.scss';
 
 const LeftBar = () => {
   return (
--- src/components/RightBar/RightBar.jsx
+++ src/components/RightBar/RightBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './RightBar/scss';
 
 const RightBar = () => {
   return (
