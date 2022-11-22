a72aead4a55b3bc2523ced893232f626b0a7ae11
marts <martssilvapro@gmail.com>
2022-11-22 14:37:50 -0300

    implements protected route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
   RouterProvider,
   Route,
   Outlet,
+  Navigate,
 } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
@@ -16,6 +17,8 @@ import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 
 function App() {
+
+  const currentUser = true;
 
   const Layout = () => {
     return (
@@ -30,10 +33,22 @@ function App() {
     );
   };
 
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />
+    };
+
+    return children
+  };
+
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
       children: [
         {
           path: "/",
