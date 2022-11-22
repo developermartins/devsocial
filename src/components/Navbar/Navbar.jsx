e51f9e5d0d3dd6a695e2590a71e88b7bdd98679a
marts <martssilvapro@gmail.com>
2022-11-22 14:48:06 -0300

    import icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import './Navbar.scss';
+
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='navbar'>
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>DevSocial</span>
+        </Link>
+      </div>
+      <div className="right"></div>
+    </nav>
   );
 };
 
