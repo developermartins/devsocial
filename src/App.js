ee2b2e00764268e601d87a0a2a41ffe4a6db1404
marts <martssilvapro@gmail.com>
2022-11-23 17:58:09 -0300

    add center initial style

--- src/App.js
+++ src/App.js
@@ -26,7 +26,9 @@ function App() {
         <Navbar />
         <div style={{ display: "flex" }}>
             <LeftBar />
-            <Outlet />
+            <div style={{ flex: 6 }}>
+              <Outlet />
+            </div>
             <RightBar />
         </div>
       </section>
