7532539ff5d0577bf31492b5c5706ef42f1c5b9b
marts <martssilvapro@gmail.com>
2022-11-23 18:24:17 -0300

    implemets style & RightBar item

--- src/components/RightBar/RightBar.jsx
+++ src/components/RightBar/RightBar.jsx
@@ -4,7 +4,31 @@ import './RightBar.scss';
 const RightBar = () => {
   return (
    <aside className="rightBar">
-      RightBar
+      <div className="container">
+        <div className="item">
+          <span>Suggestions For You</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://avatars.githubusercontent.com/u/97063329?v=4" alt="User avatar" />
+              <span>Sr.Doe</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://avatars.githubusercontent.com/u/97063329?v=4" alt="User avatar" />
+              <span>Sr.Doe</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+        </div>
+      </div>
    </aside>
   );
 };
