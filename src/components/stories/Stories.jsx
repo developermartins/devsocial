d07a6014a5f8f1fb975d52135b5605cd1d5e3877
marts <martssilvapro@gmail.com>
2022-12-01 14:12:25 -0300

    add basic story & post

--- src/components/stories/Stories.jsx
+++ src/components/stories/Stories.jsx
@@ -2,8 +2,39 @@ import React from 'react';
 import './Stories.scss';
 
 const Stories = () => {
+
+  const stories = [
+    {
+      id: 1,
+      name: "John Doe",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 2,
+      name: "John Doe",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 3,
+      name: "John Doe",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 4,
+      name: "John Doe",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+  ];
+
   return (
-    <div>Stories</div>
+    <section className="stories">
+      { stories.map((story) => (
+        <div className="story">
+          <img src={ story.img } alt="profile story" />
+          <span>{ story.name }</span>
+        </div>
+      )) }
+    </section>
   );
 };
 
