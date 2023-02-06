5ae425b641ae0e9a7d4118f4c17edcdccbb30c2f
devLucasmarts <martssilvapro@gmail.com>
2023-02-06 14:45:20 -0300

    add images alt

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -15,8 +15,8 @@ const Profile = () => {
   return (
     <section className='profile'>
       <div className="images">
-        <img src="https://images.pexels.com/photos/13440765/pexels-photo-13440765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" className='cover' />
-        <img src="https://images.pexels.com/photos/14028501/pexels-photo-14028501.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" className='profilePic' />
+        <img src="https://images.pexels.com/photos/13440765/pexels-photo-13440765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="cover" className='cover' />
+        <img src="https://images.pexels.com/photos/14028501/pexels-photo-14028501.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="profile picture" className='profilePic' />
       </div>
       <div className="profile-container">
         <div className="user-info">
