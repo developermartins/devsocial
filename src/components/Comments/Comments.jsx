988d04da9a50b542fbf044fc99aa609c699dc1e7
devLucasmarts <martssilvapro@gmail.com>
2023-02-06 11:14:54 -0300

    add basic comment html structure

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,8 +1,39 @@
 import './comments.scss';
 
 const Comments = () => {
+  //Temporary
+  const comments = [
+     {
+       id: 1,
+       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
+       name: "John Doe",
+       userId: 1,
+       profilePicture:
+         "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+     },
+     {
+       id: 2,
+       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
+       name: "Jane Doe",
+       userId: 2,
+       profilePicture:
+         "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+     },
+   ];
+
   return (
-    <div>Comments</div>
+    <section className="comments">
+     { comments.map((comment) => (
+          <div key={ comment.id } className='comment'>
+               <img src={ comment.profilePicture } alt="Profile picture" />
+               <div className="info">
+                    <span>{ comment.name }</span>
+                    <p>{ comment.desc }</p>
+               </div>
+               <span className='date'>1 hour ago</span>
+          </div>
+     )) }
+    </section>
   );
 };
 
