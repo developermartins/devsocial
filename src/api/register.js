eff5de5f37e0866c7045928e391e9ba457c81199
devLucasmarts <martssilvapro@gmail.com>
2023-02-13 16:24:58 -0300

    add await

--- src/api/register.js
+++ src/api/register.js
@@ -1,7 +1,7 @@
 import { api } from "./api";
 
 export const registerUser = async (data) => {
-     const res = api.post("/auth/register", data);
+     const res = await api.post("/auth/register", data);
 
      return res;
 };
