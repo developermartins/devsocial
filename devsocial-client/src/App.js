bc7ff87cb805af913f7f3b5e79217dd2eee418c7
devLucasmarts <martssilvapro@gmail.com>
2023-03-03 09:52:53 -0300

    remove jsconfig

--- devsocial-client/src/App.js
+++ devsocial-client/src/App.js
@@ -1,9 +1,18 @@
-import './index.css';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import HomePage from 'scenes/homePage';
+import LoginPage from 'scenes/loginPage';
+import ProfilePage from 'scenes/profilePage';
 
 const App = () => {
   return (
     <section className='app'>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ LoginPage } />
+          <Route path='/home' element={ HomePage } />
+          <Route path='/profile/:useId' element={ ProfilePage } />
+        </Routes>
+      </BrowserRouter>
     </section>
   )
 }
