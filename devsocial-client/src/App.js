1d20986f015cec17e961ffb558b3b74d818b5f85
devLucasmarts <martssilvapro@gmail.com>
2023-03-03 09:58:04 -0300

    fix component impot

--- devsocial-client/src/App.js
+++ devsocial-client/src/App.js
@@ -1,20 +1,20 @@
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
-import HomePage from 'scenes/homePage';
-import LoginPage from 'scenes/loginPage';
-import ProfilePage from 'scenes/profilePage';
+import HomePage from './scenes/homePage';
+import LoginPage from './scenes/loginPage';
+import ProfilePage from './scenes/profilePage';
 
 const App = () => {
   return (
     <section className='app'>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={ LoginPage } />
-          <Route path='/home' element={ HomePage } />
-          <Route path='/profile/:useId' element={ ProfilePage } />
+          <Route path='/' element={ <LoginPage /> } />
+          <Route path='/home' element={ <HomePage /> } />
+          <Route path='/profile/:useId' element={ <ProfilePage /> } />
         </Routes>
       </BrowserRouter>
     </section>
-  )
-}
+  );
+};
 
-export default App
+export default App;
