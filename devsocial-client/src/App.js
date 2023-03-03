d98acdd1fe4f89e7f834c25d3c4e2fbdb17360e5
devLucasmarts <martssilvapro@gmail.com>
2023-03-03 14:43:58 -0300

    add theme config on App

--- devsocial-client/src/App.js
+++ devsocial-client/src/App.js
@@ -1,17 +1,30 @@
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme';
+
 import HomePage from './scenes/homePage';
 import LoginPage from './scenes/loginPage';
 import ProfilePage from './scenes/profilePage';
 
 const App = () => {
+
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <section className='app'>
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={ <LoginPage /> } />
-          <Route path='/home' element={ <HomePage /> } />
-          <Route path='/profile/:useId' element={ <ProfilePage /> } />
-        </Routes>
+        <ThemeProvider theme={ theme }>
+          <CssBaseline />
+          <Routes>
+            <Route path='/' element={ <LoginPage /> } />
+            <Route path='/home' element={ <HomePage /> } />
+            <Route path='/profile/:useId' element={ <ProfilePage /> } />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </section>
   );
