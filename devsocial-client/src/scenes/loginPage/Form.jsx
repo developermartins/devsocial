30555c13df3c13569ccfcedf356e9a105f5a0363
devLucasmarts <martssilvapro@gmail.com>
2023-03-06 08:39:23 -0300

    add register validation schema

--- devsocial-client/src/scenes/loginPage/Form.jsx
+++ devsocial-client/src/scenes/loginPage/Form.jsx
@@ -17,6 +17,16 @@ import * as yup from "yup";
 import DropZone from "react-dropzone";
 import FlexBetween from "../../components/FlexBetween";
 
+const registerSchema = yup.object().shape({
+  firstName: yup.string().required("required"),
+  lastName: yup.string().required("required"),
+  email: yup.string().email("Invalid email").required("required"),
+  password: yup.string().required("required"),
+  location: yup.string().required("required"),
+  occupation: yup.string().required("required"),
+  picture: yup.string().required("required"),
+})
+
 const Form = () => {
   return (
     <div>Form</div>
