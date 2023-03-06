c0601a94ba33b9b292cd885811e21d005a24bd27
devLucasmarts <martssilvapro@gmail.com>
2023-03-06 10:17:09 -0300

    implements navigation form

--- devsocial-client/src/scenes/loginPage/Form.jsx
+++ devsocial-client/src/scenes/loginPage/Form.jsx
@@ -216,13 +216,32 @@ const Form = () => {
               sx={{
                 m: "2rem 0",
                 p: "1rem",
-                backgroundColor: palette.primary,main,
+                backgroundColor: palette.primary.main,
                 color: palette.background.alt,
                 "&:hover": { color: palette.primary.main }
               }}
             >
               { isLogin ? "LOGIN" : "REGISTER" }
             </Button>
+            <Typography
+              onClick={() => {
+                setPageType(isLogin ? "register" : "login");
+                resetForm();
+              }}
+              sx={{
+                textDecoration: "underline",
+                color: palette.primary.main,
+                "&:hover": {
+                  cursor: "pointer",
+                  color: palette.primary.light,
+                }
+              }}
+            >
+              { isLogin
+                ? "Don't have an account? Sign Up here."
+                : "Already have an account? Login here."
+              }
+            </Typography>
           </Box>
         </form>
       )}
