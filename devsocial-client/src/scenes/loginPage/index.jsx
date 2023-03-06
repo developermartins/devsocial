52bac56604484917d144dfca3347837f7a3c98e2
devLucasmarts <martssilvapro@gmail.com>
2023-03-06 08:25:30 -0300

    implements basic login template

--- devsocial-client/src/scenes/loginPage/index.jsx
+++ devsocial-client/src/scenes/loginPage/index.jsx
@@ -1,13 +1,43 @@
 import {	Box, Typography, useTheme, useMediaQuery	} from '@mui/material';
 
-const loginPage = () => {
-	
+const LoginPage = () => {
+	const theme = useTheme();
+	const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
 
 	return (
-		<section>
-			login
-		</section>
+		<Box>
+			<Box
+				width="100%"
+				backGroundColor={ theme.palette.background.alt }
+				p="1rem 6%"
+				textAlign="center"
+			>
+				<Typography 
+					fontWeight="bold"
+					fontSize="32px"
+					color="primary"
+				>
+						devsocial
+				</Typography>
+			</Box>
+
+			<Box 
+				width={ isNonMobileScreens ? "50%" : "93%" }
+				p="2rem"
+				m="2rem auto"
+				borderRadius="1.5rem"
+				backGroundColor={ theme.palette.background.alt }
+			>
+				<Typography
+					fontWeight="500"
+					variant='h5'
+					sx={{	mb: "1.5rem" }}
+				>
+					Welcome to devsocial!
+				</Typography>
+			</Box>
+		</Box>
 	);
 };
    
-export default loginPage;
+export default LoginPage;
