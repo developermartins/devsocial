caca12d0118ddaabd147b3aca00301a4bf6b11b3
devLucasmarts <martssilvapro@gmail.com>
2023-03-07 15:32:17 -0300

    import components

--- devsocial-client/src/scenes/widgets/UserWidget.jsx
+++ devsocial-client/src/scenes/widgets/UserWidget.jsx
@@ -3,4 +3,9 @@ import {
   EditOutlined,
   LocationOnOutlined,
   WorkOutlineOutlined,
-} from "@mui/icons-material";
+} from '@mui/icons-material';
+import { Box, Typography, Divider, useTheme } from '@mui/material';
+
+import UserImage from '../../components/UserImage';
+import FlexBetween from '../../components/FlexBetween';
+import WidgetWrapper from '../../components/WidgetWrapper';
