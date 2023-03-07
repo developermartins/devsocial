4fd6303a61dd38472b7bb9300b40492e92c6fe37
devLucasmarts <martssilvapro@gmail.com>
2023-03-07 15:49:32 -0300

    return data

--- devsocial-client/src/api/user.js
+++ devsocial-client/src/api/user.js
@@ -3,5 +3,5 @@ import { api } from "./api";
 export const getUser = async (userId, token) => {
   const res = api.get(`users/${userId}`, { headers: { Authorization: `Bearer ${token}` } });
 
-  return res;
+  return res.data;
 };
