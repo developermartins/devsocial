f9c302e2c87e8cf8a6732d2c2a98dcc1ece5de11
devLucasmarts <martssilvapro@gmail.com>
2023-03-07 15:34:02 -0300

    import deps

--- devsocial-client/src/scenes/widgets/UserWidget.jsx
+++ devsocial-client/src/scenes/widgets/UserWidget.jsx
@@ -5,7 +5,11 @@ import {
   WorkOutlineOutlined,
 } from '@mui/icons-material';
 import { Box, Typography, Divider, useTheme } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import UserImage from '../../components/UserImage';
 import FlexBetween from '../../components/FlexBetween';
 import WidgetWrapper from '../../components/WidgetWrapper';
+
