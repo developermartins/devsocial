5800894ac3eb50d06a74813b38e74ee8f92c49d1
devLucasmarts <martssilvapro@gmail.com>
2023-03-08 19:39:00 -0300

    change function name

--- devsocial-client/src/api/posts.js
+++ devsocial-client/src/api/posts.js
@@ -1,6 +1,6 @@
 import { api } from "./api";
 
-export const getUser = async (formData, token) => {
+export const createPost = async (formData, token) => {
   const res = api.post(`posts`, formData, { headers: { Authorization: `Bearer ${token}` } });
 
   return res;
