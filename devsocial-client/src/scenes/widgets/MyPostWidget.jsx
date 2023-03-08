a12483c5a8865ada33e09794dc74e05509c3b4e4
devLucasmarts <martssilvapro@gmail.com>
2023-03-08 19:36:07 -0300

    setup basic functions

--- devsocial-client/src/scenes/widgets/MyPostWidget.jsx
+++ devsocial-client/src/scenes/widgets/MyPostWidget.jsx
@@ -26,7 +26,32 @@ import Dropzone from "react-dropzone";
 import UserImage from "../../components/UserImage";
 import WidgetWrapper from "../../components/WidgetWrapper";
 
-const MyPostWidget = () => {
+const MyPostWidget = ({ picturePath }) => {
+  const dispatch = useDispatch();
+  const [isImage, setIsImage] = useState(false);
+  const [image, setImage] = useState(null);
+  const [post, setPost] = useState("");
+  const { palette } = useTheme();
+  const { _id } = useSelector((state) => state.user);
+  const { token } = useSelector((state) => state.token);
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const mediumMain = palette.neutral.mediumMain;
+  const medium = palette.neutral.medium;
+
+  const handlePost = async () => {
+    const formData = new FormData();
+
+    formData.append("userId", _id);
+    formData.append("description", post);
+    
+    if (image) {
+      formData.append("picture", image);
+      formData.append("picturePath", image.name);
+    };
+
+    
+  };
+
   return (
     <div>MyPostWidget</div>
   );
