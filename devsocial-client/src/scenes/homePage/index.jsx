02d8a0bfeb9899f8cba1310a7fb9d9f29571b366
devLucasmarts <martssilvapro@gmail.com>
2023-03-08 19:19:02 -0300

    add MyPostWidget component

--- devsocial-client/src/scenes/homePage/index.jsx
+++ devsocial-client/src/scenes/homePage/index.jsx
@@ -2,6 +2,7 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "../navbar";
 import UserWidget from "../widgets/UserWidget";
+import MyPostWidget from "../../scenes/widgets/MyPostWidget";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -27,7 +28,7 @@ const HomePage = () => {
           flexBasis={ isNonMobileScreens ? "42%" : undefined }
           mt={ isNonMobileScreens ? undefined : "2rem" }
         >
-
+          <MyPostWidget picturePath={ picturePath } />
         </Box>
         { isNonMobileScreens && (
           <Box flexBasis="26%">
