6b3944fecc32c53a2b34d3213de27eae5c55e7a4
devLucasmarts <martssilvapro@gmail.com>
2023-03-09 15:24:47 -0300

    AdvertWidget completed

--- devsocial-client/src/scenes/widgets/AdvertWidget.jsx
+++ devsocial-client/src/scenes/widgets/AdvertWidget.jsx
@@ -4,8 +4,38 @@ import FlexBetween from "../../components/FlexBetween";
 import WidgetWrapper from "../../components/WidgetWrapper";
 
 const AdvertWidget = () => {
+  const { palette } = useTheme();
+
+  const dark = palette.neutral.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+
   return (
-    <div>AdvertWidget</div>
+    <WidgetWrapper>
+      <FlexBetween>
+        <Typography color={ dark } variant="h5" fontWeight="500">
+          Sponsored
+        </Typography>
+
+        <Typography color={ medium }>Create Ad</Typography>
+      </FlexBetween>
+      <img
+        width="100%"
+        height="auto"
+        src="http://localhost:3001/assets/info4.jpeg"
+        alt="advert"
+        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      />
+
+      <FlexBetween>
+        <Typography color={ main }>MikaCosmetics</Typography>
+        <Typography color={ medium }>mikacosmetics.com</Typography>
+      </FlexBetween>
+      <Typography color={ medium } m="0.5rem 0">
+        Your pathway to stunning and immaculate beauty and made sure your skin
+        is exfoliating skin and shining like light.
+      </Typography>
+    </WidgetWrapper>
   );
 };
 
