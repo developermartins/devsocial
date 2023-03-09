df0460e7b0db84c279ecd39704912d3553df087c
devLucasmarts <martssilvapro@gmail.com>
2023-03-09 07:56:49 -0300

    setup PostsWidget component

--- devsocial-client/src/scenes/widgets/PostsWidget.jsx
+++ devsocial-client/src/scenes/widgets/PostsWidget.jsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "../../state";
 
+import PostWidget from "./PostWidget";
 
-const PostsWidget = () => {
+const PostsWidget = ({ userId, isProfile = false }) => {
+  const dispatch = useDispatch();
+
+  const posts = useSelector((state) => state.posts);
+  const token = useSelector((state) => state.token);
+
+  const getFeedPosts = async () => {
+    
+  };
+
   return (
     <div>PostsWidget</div>
   );
