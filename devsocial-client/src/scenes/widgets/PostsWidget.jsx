b2c1369e263dc2809d766f9dab2ef628e4a6795d
devLucasmarts <martssilvapro@gmail.com>
2023-03-09 08:15:24 -0300

    add PostWidget

--- devsocial-client/src/scenes/widgets/PostsWidget.jsx
+++ devsocial-client/src/scenes/widgets/PostsWidget.jsx
@@ -32,7 +32,35 @@ const PostsWidget = ({ userId, isProfile = false }) => {
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <div>PostsWidget</div>
+    <>
+      { posts.map(
+        ({
+          _id,
+          userId,
+          firstName,
+          lastName,
+          description,
+          location,
+          picturePath,
+          userPicturePath,
+          likes,
+          comments,
+        }) => (
+          <PostWidget
+            key={ _id }
+            postId={ _id }
+            postUserId={ userId }
+            name={ `${firstName} ${lastName}` }
+            description={ description }
+            location={ location }
+            picturePath={ picturePath }
+            userPicturePath={ userPicturePath }
+            likes={ likes }
+            comments={ comments }
+          />
+        )
+      ) }
+    </>
   );
 };
 
