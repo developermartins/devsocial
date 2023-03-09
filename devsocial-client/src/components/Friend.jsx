9da368c7a8c0a64b08fda5b606d1a27e2dfc28aa
devLucasmarts <martssilvapro@gmail.com>
2023-03-09 10:13:45 -0300

    Friend component completed

--- devsocial-client/src/components/Friend.jsx
+++ devsocial-client/src/components/Friend.jsx
@@ -2,7 +2,7 @@ import { PersonAddOutlined, PersonRemoveOutlined } from "@mui/icons-material";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { getFriend } from "../api/user";
+import { addRemoveFriend } from "../api/user";
 import { setFriends } from "../state";
 
 import FlexBetween from "./FlexBetween";
@@ -30,7 +30,48 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
   };
 
   return (
-    <div>Friend</div>
+    <FlexBetween>
+      <FlexBetween gap="1rem">
+        <UserImage image={ userPicturePath } size="55px" />
+        <Box
+          onClick={() => {
+            navigate(`/profile/${friendId}`)
+            navigate(0)
+          }}
+        >
+          <Typography
+            color={ main }
+            variant="h5"
+            fontWeight="500"
+            sx={{
+              "&:hover": {
+                color: palette.primary.light,
+                cursor: "pointer"
+              }
+            }}
+          >
+            { name }
+          </Typography>
+          <Typography color={ medium } fontSize="0.75rem">
+            { subtitle }
+          </Typography>
+        </Box>
+      </FlexBetween>
+
+      <IconButton
+        onClick={() => patchFriend()}
+        sx={{
+          backgroundColor: primaryLight,
+          p: "0.6rem"
+        }}
+      >
+        { isFriend ? (
+          <PersonRemoveOutlined sx={{ color: primaryDark }} />
+        ) : (
+          <PersonAddOutlined sx={{ color: primaryDark }} />
+        ) }
+      </IconButton>
+    </FlexBetween>
   );
 };
 
