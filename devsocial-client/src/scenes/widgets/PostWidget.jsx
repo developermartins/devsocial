1f9362e45d0d13fe2b945fce90b48c6a55b3aa2b
devLucasmarts <martssilvapro@gmail.com>
2023-03-09 08:19:51 -0300

    import deps

--- devsocial-client/src/scenes/widgets/PostWidget.jsx
+++ devsocial-client/src/scenes/widgets/PostWidget.jsx
@@ -1,4 +1,16 @@
+import {
+  ChatBubbleOutlineOutlined,
+  FavoriteBorderOutlined,
+  FavoriteOutlined,
+  ShareOutlined,
+} from "@mui/icons-material";
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from "state";
 
+import FlexBetween from "../../components/FlexBetween";
+import Friend from "../../components/Friend";
 
 const PostWidget = () => {
   return (
