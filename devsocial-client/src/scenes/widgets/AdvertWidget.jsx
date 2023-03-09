83cf6ab06e534b3864b33c8937b70765974f6d1d
devLucasmarts <martssilvapro@gmail.com>
2023-03-09 15:13:53 -0300

    add imports

--- devsocial-client/src/scenes/widgets/AdvertWidget.jsx
+++ devsocial-client/src/scenes/widgets/AdvertWidget.jsx
@@ -1,4 +1,7 @@
+import { Typography, useTheme } from "@mui/material";
 
+import FlexBetween from "../../components/FlexBetween";
+import WidgetWrapper from "../../components/WidgetWrapper";
 
 const AdvertWidget = () => {
   return (
