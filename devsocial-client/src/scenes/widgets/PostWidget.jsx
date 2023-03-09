8cf0dca13ff5a5c78bce24ac0e03fb99268a5704
devLucasmarts <martssilvapro@gmail.com>
2023-03-09 11:10:03 -0300

    PostWidget completed

--- devsocial-client/src/scenes/widgets/PostWidget.jsx
+++ devsocial-client/src/scenes/widgets/PostWidget.jsx
@@ -12,6 +12,7 @@ import { likeOrDislikePost } from "../../api/posts";
 
 import FlexBetween from "../../components/FlexBetween";
 import Friend from "../../components/Friend";
+import WidgetWrapper from "../../components/WidgetWrapper";
 
 const PostWidget = ({
   postId,
@@ -42,7 +43,67 @@ const PostWidget = ({
   };
 
   return (
-    <div>PostWidget</div>
+    <WidgetWrapper m="2rem 0">
+      <Friend
+        friendId={ postUserId }
+        name={ name }
+        subtitle={ location }
+        userPicturePath={ userPicturePath }
+      />
+      <Typography color={ main } sx={{ mt: "1rem" }}>
+        { description }
+      </Typography>
+      { picturePath && (
+        <img 
+          width="100%"
+          height="auto"
+          alt="post"
+          style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
+          src={`http://localhost:3001/assets/${picturePath}`}
+        />
+      ) }
+
+      <FlexBetween mt="0.25rem">
+        <FlexBetween gap="1rem">
+
+          <FlexBetween gap="3rem">
+            <IconButton onClick={patchLike}>
+              { isLiked ? (
+                <FavoriteOutlined sx={{ color: primary }} />
+              ) : (
+                <FavoriteBorderOutlined />
+              ) }
+            </IconButton>
+            <Typography>{ likeCount }</Typography>
+          </FlexBetween>
+
+          <FlexBetween gap="0.3rem">
+            <IconButton onClick={() => setIsComments(!isComments)}>
+              <ChatBubbleOutlineOutlined />
+            </IconButton>
+            <Typography>{ comments.length }</Typography>
+          </FlexBetween>
+
+        </FlexBetween>
+
+        <IconButton>
+          <ShareOutlined />
+        </IconButton>
+      </FlexBetween>
+      { isComments && (
+        <Box mt="0.5rem">
+          { comments.map((comment, index) => (
+            <Box key={`${name}-${index}`}>
+              <Divider />
+              <Typography sx={{ color: main, m: "0.5rem 0", pl: "1rem" }}>
+                { comment }
+              </Typography>
+            </Box>
+          )) }
+          <Divider />
+        </Box>
+      ) }
+    </WidgetWrapper>
   );
 };
 
