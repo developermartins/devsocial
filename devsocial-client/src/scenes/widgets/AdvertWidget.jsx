7bf71bf6fe4c96ee867c65279c08fd35d0f7a7c8
devLucasmarts <martssilvapro@gmail.com>
2023-03-10 10:11:28 -0300

    update AdvertWidget

--- devsocial-client/src/scenes/widgets/AdvertWidget.jsx
+++ devsocial-client/src/scenes/widgets/AdvertWidget.jsx
@@ -22,18 +22,18 @@ const AdvertWidget = () => {
       <img
         width="100%"
         height="auto"
-        src="http://localhost:3001/assets/info4.jpeg"
+        src="http://localhost:3001/assets/MidjourneyEmblem.png"
         alt="advert"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
 
       <FlexBetween>
-        <Typography color={ main }>MikaCosmetics</Typography>
-        <Typography color={ medium }>mikacosmetics.com</Typography>
+        <Typography color={ main }>Midjourney</Typography>
+        <Typography color={ medium }>midjourney.com</Typography>
       </FlexBetween>
       <Typography color={ medium } m="0.5rem 0">
-        Your pathway to stunning and immaculate beauty and made sure your skin
-        is exfoliating skin and shining like light.
+        Midjourney is an independent research lab exploring new mediums of thought and expanding
+        the imaginative powers of the human species.
       </Typography>
     </WidgetWrapper>
   );
