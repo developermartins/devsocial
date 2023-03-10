167e2c2c0fd8166dbf4e062412dd27dc9a96d78a
devLucasmarts <martssilvapro@gmail.com>
2023-03-10 08:26:31 -0300

    fix bugs

--- devsocial-client/src/scenes/profilePage/index.jsx
+++ devsocial-client/src/scenes/profilePage/index.jsx
@@ -4,13 +4,13 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getUser } from "../../api/user";
 
-import Navbar from "../scenes/navbar";
-import FriendListWidget from "../scenes/widgets/FriendListWidget";
-import MyPostWidget from "../scenes/widgets/MyPostWidget";
-import PostsWidget from "../scenes/widgets/PostsWidget";
-import UserWidget from "../scenes/widgets/UserWidget";
+import Navbar from "../navbar";
+import FriendListWidget from "../widgets/FriendListWidget";
+import MyPostWidget from "../widgets/MyPostWidget";
+import PostsWidget from "../widgets/PostsWidget";
+import UserWidget from "../widgets/UserWidget";
 
-const profilePage = () => {
+const ProfilePage = () => {
 	const [user, setUser] = useState(null);
 	const {	userId	} = useParams();
 	const token = useSelector((state) => state.token);
@@ -33,4 +33,4 @@ const profilePage = () => {
   );
 };
 
-export default profilePage;
+export default ProfilePage;
